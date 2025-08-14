c6121c46ee59846506c640891f95898a8074a62d
Ethan Bradway <ebradway225@gmail.com>
2025-08-13 20:58:01 -0400

    enabling bluetooth
    https://nixos.wiki/wiki/Bluetooth

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,16 @@
     #media-session.enable = true;
   };
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
