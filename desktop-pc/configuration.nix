20558cea6de722493aa76ec1443a963beb9f5caa
Ethan Bradway <ebradway225@gmail.com>
2025-10-09 11:07:52 -0400

    disabling fine-grained power management for desktop PC

--- desktop-pc/configuration.nix
+++ desktop-pc/configuration.nix
@@ -78,7 +78,7 @@
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
 
     # Use the Nvidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
