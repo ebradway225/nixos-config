9649316264538b45e0d15deef18039ccc8e57350
Ethan Bradway <ebradway225@gmail.com>
2025-10-08 21:49:08 -0400

    moving laptop-specific config files to their own folder
    https://wiki.nixos.org/wiki/NixOS_system_configuration#Defining_NixOS_as_a_flake
    https://github.com/dreamsofautonomy/homelab
    https://youtu.be/2yplBzPCghA?si=J_E43WS7UhvaTHMe
    https://www.reddit.com/r/NixOS/comments/ppl19u/how_to_manage_multiple_different_machines_using/

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       nixos = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./asus-gaming-laptop/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
