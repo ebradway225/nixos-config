9728075e3fbbb91f898081c2d8a4b781941bb8d9
Ethan Bradway <ebradway225@gmail.com>
2025-10-09 08:27:08 -0400

    removing NVIDIA Optimus from desktop PC configuration
    https://nixos.wiki/wiki/Nvidia#Optimus_PRIME_Option_A:_Offload_Mode

--- desktop-pc/configuration.nix
+++ desktop-pc/configuration.nix
@@ -90,16 +90,6 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    prime = {
-      intelBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:0:2:0";
-
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-    };
   };
 
   # Enable CUPS to print documents.
