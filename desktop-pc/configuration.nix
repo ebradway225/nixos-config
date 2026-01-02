80945eae15de3944848618ddb89c8fefb5fe259c
Ethan Bradway <ebradway225@gmail.com>
2026-01-02 17:13:27 -0500

    updating timezone for desktop

--- desktop-pc/configuration.nix
+++ desktop-pc/configuration.nix
@@ -29,7 +29,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "America/Denver";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
